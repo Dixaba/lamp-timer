7957f8373ecc22dc83dd3d0e711a3e02093a0933
Dixaba <dixaba@yandex.ru>
2019-06-27 20:06:23 +0300

    Done

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <avr/io.h>
-// #include <avr/sleep.h>
+#include <avr/sleep.h>
 #include <avr/interrupt.h>
 
 #define BUTTON PB4
@@ -9,89 +9,98 @@
 
 // 10 second counter
 volatile uint16_t timerCounter;
+volatile bool buttonPressed;
+volatile bool step;
 
-volatile uint16_t timeStamps[2] = {0, 0};
+uint16_t timeStamps[2] = {0, 0};
+uint16_t switches[2] = {0, 0};
+
+uint8_t done;
 
 uint16_t time;
+uint16_t random_number = 0;
 
 bool isConfigured;
-bool isActive;
 
-volatile bool buttonPressed;
+uint16_t lfsr16_next(uint16_t n)
+{
+  return (n >> 0x01U) ^ (-(n & 0x01U) & 0xB400U);
+}
 
-volatile bool step;
+void random_init()
+{
+  random_number = 12345;
+}
+
+int16_t random()
+{
+  random_number = lfsr16_next(random_number);
+  return (random_number % 1024 - 512) / 2;
+}
 
 ISR(TIM0_OVF_vect)
 {
   // Debounce
-  // static uint8_t currState = 0;
-  // static uint8_t lastState = 0;
-  // uint8_t state = PINB & (1 << BUTTON);
-  // static uint8_t count = 0;
-  // if (state != lastState)
-  //   {
-  //     count = 5;
-  //   }
-  // if (count > 0)
-  //   {
-  //     count--;
-  //   }
-  // else
-  //   {
-  //     if (state != currState)
-  //       {
-  //         currState = state;
-  //         if (currState == 1)
-  //           {
-  //             buttonPressed = true;
-  //           }
-  //       }
-  //   }
-  // lastState = state;
+  if (!isConfigured)
+    {
+      static uint8_t currState = PINB & (1 << BUTTON);
+      static uint8_t lastState = currState;
+      static uint16_t count = 0;
+      uint8_t state = PINB & (1 << BUTTON);
+
+      if (state != lastState)
+        {
+          count = 100;
+        }
+
+      if (count > 0)
+        {
+          count--;
+        }
+      else
+        {
+          if (state != currState)
+            {
+              currState = state;
+
+              if (currState > 0)
+                {
+                  buttonPressed = true;
+                }
+            }
+        }
+
+      lastState = state;
+    }
+
   timerCounter++;
 
-  if (timerCounter == 1) // ~10 seconds
+  if (timerCounter == 40150) // 10 seconds
     {
       timerCounter = 0;
       step = true;
     }
 }
 
-uint16_t rand2()
-{
-  // Start convertion
-  ADCSRA |= (1 << ADSC);
-
-  // Wait for convertion to complete
-  while (ADCSRA & (1 << ADSC));
-
-  return ADC;
-}
-
 int main()
 {
-  cli();
-  // set_sleep_mode(SLEEP_MODE_IDLE);
-  // sleep_enable();
-  // clock frequency / 1024
-  TCCR0B = (1 << CS02) | (1 << CS00);
+  set_sleep_mode(SLEEP_MODE_IDLE);
+  sleep_enable();
+  // clock frequency / 1
+  TCCR0B = (1 << CS01);
   TIMSK0 |= (1 << TOIE0);
   // Set LED and system outputs as outputs
   DDRB |= (1 << LED) | (1 << OUTPUT);
   // Write 0
-  PORTB &= ~((1 << LED) | (1 << OUTPUT));
-  // AREF = AVcc, port selected by defines
-  // ADMUX = (1 << REFS0) | (RANDOM);
-  // // ADC Enable and prescaler of 128
-  // ADCSRA = (1 << ADEN) |
-  //          (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+  PORTB |= (1 << LED);
+  ADCSRA &= ~(1 << ADEN);
   // Next command will change prescaler
   CLKPR = (1 << CLKPCE);
   // Set prescaler 256
-  CLKPR = (0 << CLKPCE) |
-          (0 << CLKPS3) | (1 << CLKPS2) | (1 << CLKPS1) | (0 << CLKPS0);
-  timerCounter = time = 0;
-  isConfigured = isActive = buttonPressed = false;
+  CLKPR = (0 << CLKPCE) | (0 << CLKPS3);
+  random_init();
+  timerCounter = time = done = 0;
+  isConfigured = buttonPressed = false;
   sei();
 
   while (1)
@@ -99,14 +108,41 @@ int main()
       if (step)
         {
           step = false;
-          PORTB ^= (1 << LED);
+          time++;
+
+          if (time == 8640)
+            {
+              time = 0;
+              switches[0] = timeStamps[0] + random();
+              switches[1] = timeStamps[1] + random();
+            }
+
+          if (isConfigured)
+            {
+              PORTB ^= (1 << LED);
+
+              if (time == timeStamps[0])
+                {
+                  PORTB |= (1 << OUTPUT);
+                }
+              else
+                if (time == timeStamps[1])
+                  {
+                    PORTB &= ~(1 << OUTPUT);
+                  }
+            }
         }
 
-      // if (buttonPressed)
-      //   {
-      //     buttonPressed = false;
-      //     PORTB ^= (1 << LED);
-      //   }
-      // sleep_cpu(); //и в самом конце цикла - уходим в сон.
+      if (buttonPressed && !isConfigured)
+        {
+          buttonPressed = false;
+          timeStamps[done] = time;
+          switches[done] = time + random();
+          done++;
+          PORTB ^= ((1 << OUTPUT));
+          isConfigured = (done == 2);
+        }
+
+      sleep_cpu(); //и в самом конце цикла - уходим в сон.
     }
 }

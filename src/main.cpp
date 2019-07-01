4b5ce6ad96771b9d5546b098407fea5bb6bd3605
Dixaba <dixaba@yandex.ru>
2019-07-01 12:17:31 +0300

    Comments

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,25 @@
 #define BUTTON PB4
 #define LED PB3
 #define OUTPUT PB1
-#define RANDOM 1
 
-// 10 second counter
+// Flags
 volatile uint16_t timerCounter;
 volatile bool buttonPressed;
 volatile bool step;
 
+// Time when pressed and when to switch output
 uint16_t timeStamps[2] = {0, 0};
 uint16_t switches[2] = {0, 0};
 
+// Init counter
 uint8_t done;
 
+// Time counter
 uint16_t time;
+
 uint16_t random_number = 0;
 
+// It means exactly what are you thinking about
 bool isConfigured;
 
 uint16_t lfsr16_next(uint16_t n)
@@ -38,9 +42,11 @@ int16_t random()
   return (random_number % 1024 - 512) / 2;
 }
 
+// Timer overflow interrupt
 ISR(TIM0_OVF_vect)
 {
-  // Debounce
+  // Debounce code
+  // If already configured - just ignore button
   if (!isConfigured)
     {
       static uint8_t currState = PINB & (1 << BUTTON);
@@ -73,9 +79,10 @@ ISR(TIM0_OVF_vect)
       lastState = state;
     }
 
+  // 10 seconds timer
   timerCounter++;
 
-  if (timerCounter == 40150) // 10 seconds
+  if (timerCounter == 40150) // Counted by experiment
     {
       timerCounter = 0;
       step = true;
@@ -86,41 +93,49 @@ int main()
 {
   set_sleep_mode(SLEEP_MODE_IDLE);
   sleep_enable();
-  // clock frequency / 1
+  // Timer works at clock frequency / 1
   TCCR0B = (1 << CS01);
   TIMSK0 |= (1 << TOIE0);
   // Set LED and system outputs as outputs
   DDRB |= (1 << LED) | (1 << OUTPUT);
-  // Write 0
+  // Write values
   PORTB |= (1 << LED);
   ADCSRA &= ~(1 << ADEN);
   // Next command will change prescaler
   CLKPR = (1 << CLKPCE);
-  // Set prescaler 256
+  // Set prescaler to 256
   CLKPR = (0 << CLKPCE) | (0 << CLKPS3);
+  // Init RNG
   random_init();
+  // Init variables
   timerCounter = time = done = 0;
   isConfigured = buttonPressed = false;
+  // Allow interrupts
   sei();
 
   while (1)
     {
+      // 10 seconds passed
       if (step)
         {
+          // Reset flag
           step = false;
           time++;
 
-          if (time == 8640)
+          if (time == 8640) // Exactly one day (24*60*60/10)
             {
               time = 0;
+              // Recalculate switch times with RNG
               switches[0] = timeStamps[0] + random();
               switches[1] = timeStamps[1] + random();
             }
 
           if (isConfigured)
             {
+              // Need to blink LED
               PORTB ^= (1 << LED);
 
+              // Should we switch output?
               if (time == timeStamps[0])
                 {
                   PORTB |= (1 << OUTPUT);
@@ -133,6 +148,7 @@ int main()
             }
         }
 
+      // Setup
       if (buttonPressed && !isConfigured)
         {
           buttonPressed = false;
@@ -143,6 +159,7 @@ int main()
           isConfigured = (done == 2);
         }
 
-      sleep_cpu(); //и в самом конце цикла - уходим в сон.
+      // Save battery
+      sleep_cpu();
     }
 }
